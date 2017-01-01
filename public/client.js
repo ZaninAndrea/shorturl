1861b6724d7895e08ec5dec8aa8a93576246bb2d
Zanin Andrea <zanin_andrea@outlook.com>
2017-01-01 16:29:40 +0100

    😋💦 Updated with Gomix

--- public/client.js
+++ public/client.js
@@ -4,23 +4,7 @@
 // by default, you've got jQuery,
 // add other scripts at the bottom of index.html
 
-$(function() {
-  console.log('hello world :o');
-  
-  $.get('/dreams', function(dreams) {
-    dreams.forEach(function(dream) {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-    });
+  $('#create').click(function() {
+    $("#create").innerHTML="gggggggg";
   });
 
-  $('form').submit(function(event) {
-    event.preventDefault();
-    dream = $('input').val();
-    $.post('/dreams?' + $.param({dream: dream}), function() {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-      $('input').val('');
-      $('input').focus();
-    });
-  });
-
-});
